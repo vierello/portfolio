c6f334945a5551344cf3dc074f4e4737bca375c4
vierello <vierello@hotmail.com>
2016-08-17 17:30:45 -0400

    added thunderstorm, more styling, responsive work

--- app/scripts/components/home.jsx
+++ app/scripts/components/home.jsx
@@ -16,19 +16,14 @@ var NavComponent = React.createClass({
           <div className="nav-row row-offcanvas row-offcanvas-left">
             <div className="column  sidebar-offcanvas" id="sidebar">
               <ul className="nav hidden-xs" id="lg-menu">
+                <li><h2>Andrew Vitello</h2></li>
                 <li className="nav-font active"><a href="#home/"><i className="glyphicon glyphicon-home"></i> Home</a></li>
                 <li className="nav-font"><a href="#resume/"><i className="glyphicon glyphicon-list-alt"></i> Resum&eacute;</a></li>
                 <li className="nav-font"><a href="#aboutme/"><i className="glyphicon glyphicon-user"></i> About Me</a></li>
               </ul>
-              <ul className="list-unstyled hidden-s hidden-xs" id="sidebar-footer">
-                <li>
-                  <h3>Andrew Vitello</h3>
-                  <h4>avitello75@gmail.com</h4>
-                  <a href="https://www.facebook.com/vitello1"><i className="media fa fa-facebook-official fa-2x"></i></a>
-                  <a href="https://twitter.com/vierello"><i className="media fa fa-twitter-square fa-2x"></i></a>
-                  <a href="https://github.com/vierello"><i className="media fa fa-github-square fa-2x"></i></a>
-                </li>
-              </ul>
+              <div className="col-md-offset-2 col-md-10 col-sm-offset-2 col-sm-10  col-xs-12 storm-cloud-container">
+                <div className="middle-align"><p className="av-logo"><a onClick={this.props.thunderstorm}><img src='images/av_logo.png'/></a></p></div>
+              </div>
               <ul className="nav visible-xs" id="xs-menu">
               	<li><a href="#home/" className="text-center"><i className="glyphicon glyphicon-home"></i></a></li>
                 <li><a href="#resume/" className="text-center"><i className="glyphicon glyphicon-list-alt"></i></a></li>
@@ -44,22 +39,33 @@ var NavComponent = React.createClass({
 
 var HomeComponent = React.createClass({
   thunderstorm: function(){
-    console.log('hello');
+    if(jQuery('.sky').hasClass('sky')){
+      jQuery('.sky').removeClass('sky').addClass('sky-storm');
+      jQuery('.clouds-one').removeClass('clouds-one').addClass('clouds-one-storm');
+      jQuery('.clouds-two').removeClass('clouds-two').addClass('clouds-two-storm');
+      jQuery('.clouds-three').removeClass('clouds-three').addClass('clouds-three-storm');
+    } else {
+      jQuery('.sky-storm').removeClass('sky-storm').addClass('sky');
+      jQuery('.clouds-one-storm').removeClass('clouds-one-storm').addClass('clouds-one');
+      jQuery('.clouds-two-storm').removeClass('clouds-two-storm').addClass('clouds-two');
+      jQuery('.clouds-three-storm').removeClass('clouds-three-storm').addClass('clouds-three');
+    }
   },
 
   render: function(){
     return (
     <div>
       <div className="sky row">
-        <NavComponent/>
+        <NavComponent thunderstorm={this.thunderstorm}/>
         <div>
-          <div className="clouds_one col-xs-12"></div>
-          <div className="clouds_two col-xs-12"></div>
-          <div className="clouds_three col-xs-12"></div>
+          <div className="clouds-one col-xs-12"></div>
+          <div className="clouds-two col-xs-12"></div>
+          <div className="clouds-three col-xs-12"></div>
         </div>
-        <div className="col-md-offset-2 col-md-10 col-sm-offset-2 col-sm-10  col-xs-12 storm-cloud-container">
-          <div className="storm-cloud"><p className="av-logo"><a onClick={this.thunderstorm}><img src='images/av_logo.png'/></a></p></div>
-        </div>
+        {// <div className="col-md-offset-2 col-md-10 col-sm-offset-2 col-sm-10  col-xs-12 storm-cloud-container">
+        //   <div className="storm-cloud"><p className="av-logo"><a onClick={this.thunderstorm}><img src='images/av_logo.png'/></a></p></div>
+        // </div>
+        }
       </div>
       <div className="row title-bar">
         <div className="col-xs-12">
@@ -97,15 +103,23 @@ var HomeComponent = React.createClass({
         </div>
       </div>
       <div className="row footer">
-        <footer className="col-xs-12">
-          <img className="headshot" src="images/AV-headshot.jpg"/>
-          <ul className="footer-links">
+        <footer className="footer col-xs-12">
+          <div className="headshot-div col-md-2 col-xs-6">
+            <img className="headshot" src="images/AV-headshot.jpg"/>
+          </div>
+          <ul className="col-md-2 col-xs-6 footer-links">
             <li><a href="#"><i className="media glyphicon glyphicon-user"></i>About Me</a></li>
             <li><a href="#"><i className="media glyphicon glyphicon-list-alt"></i>Resume</a></li>
             <li><a href="https://github.com/vierello"><i className="media fa fa-github-square fa-2x"></i>Github</a></li>
             <li><a href="https://www.facebook.com/vitello1"><i className="media fa fa-facebook-official fa-2x"></i>Facebook</a></li>
             <li><a href="https://twitter.com/vierello"><i className="media fa fa-twitter-square fa-2x"></i>Twitter</a></li>
           </ul>
+          <div className="col-md-offset-4 col-md-2 list-unstyled" id="sidebar-footer">
+            <div className="footer-personal-info">
+              <h3>Andrew Vitello</h3>
+              <h4 className="e-mail-address hidden-s">avitello75@gmail.com</h4>
+            </div>
+          </div>
         </footer>
       </div>
     </div>
--- app/scripts/components/projects.jsx
+++ app/scripts/components/projects.jsx
@@ -6,14 +6,15 @@ var BudgetMeComponent = React.createClass({
       <div className="budget-me">
         <a className="col-md-4 col-xs-12" href="https://vierello.github.io/budget-me/"><img className="budget-me-image" src="./images/budget-me.png" /></a>
         <div className="text-container col-md-8 col-xs-12">
-          <h1>Budget Me</h1>
+          <h1 className="budget-me-title"><p>Budget Me</p></h1>
           <p className="budget-me-text">Everyone needs a budget! From the time I
               started earning my own money while working my first job, until today,
               my parents have always told me that I need a budget to help me make
               sure that I am taking care of all my responsibilities. Budget Me is
               the final project I created during the last few weeks of The Iron Yard.
               It was quite a challenge, and really combined everything I learned
-              during my time there.</p>
+              during my time there.
+          </p>
           <div className="budget-me-tech-bar">
             <p className="budget-me-tech">Technologies Used: </p>
             <a className="budget-me-links" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Javascript</a>
@@ -31,14 +32,24 @@ var BudgetMeComponent = React.createClass({
 
 var GithubProfileComponent = React.createClass({
   render: function(){
+
+    // var $githubImage = jQuery('.github-profile-image-link');
+    //
+    // $githubImage.responsiveDom({
+    //     appendTo: '.github-profile',
+    //
+    //     mediaQuery: '(max-width: 768px)'
+    // });
+
     return (
       <div className="github-profile">
         <div className="text-container col-md-8 col-xs-12">
-          <h1>Github Profile</h1>
+          <h1 className="github-profile-title"><p>Github Profile</p></h1>
           <p className="github-profile-text">For this project I was tasked to recreate
               my Github Profile as closely as possible. We were not required to make
               it fully functional, though I tried to make it as functional as I could
-              with the knowledge I had at the time.</p>
+              with the knowledge I had at the time.
+          </p>
           <div className="github-profile-tech-bar">
             <p className="github-profile-tech">Technologies Used: </p>
             <a className="github-profile-links" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Javascript</a>
@@ -48,7 +59,7 @@ var GithubProfileComponent = React.createClass({
             <a className="github-profile-links" href="http://sass-lang.com/">Sass</a>
           </div>
         </div>
-        <a className="col-md-4 col-xs-12" href="http://vierello.github.io/4.4-github-profile/"><img className="github-profile-image" src="./images/github-profile.png" /></a>
+          <a className="github-profile-image-link col-md-4 col-xs-12" href="http://vierello.github.io/4.4-github-profile/"><img className="github-profile-image" src="./images/github-profile.png" /></a>
       </div>
     )
   }
@@ -60,12 +71,13 @@ var RecipesComponent = React.createClass({
       <div className="recipes">
         <a className="col-md-4 col-xs-12" href="https://vierello.github.io/8.4-recipes/"><img className="recipes-image" src="./images/recipes.png" /></a>
         <div className="text-container col-md-8 col-xs-12">
-          <h1>Recipes</h1>
+          <h1 className="recipes-title"><p>Recipes</p></h1>
           <p className="recipes-text">For this project the goal was to create a
               recipe book that has a way to adjust recipes based on serving size.
               To accomplish this I used React for the views and Backbone for the
               models and collections. This was also the first time we used Parse
-              to store data.</p>
+              to store data.
+          </p>
           <div className="recipes-tech-bar">
             <p className="recipes-tech">Technologies Used: </p>
             <a className="recipes-links" href="https://getbootstrap.com/">Bootstrap</a>
@@ -73,6 +85,7 @@ var RecipesComponent = React.createClass({
             <a className="recipes-links" href="https://facebook.github.io/react/index.html">React</a>
             <a className="recipes-links" href="http://sass-lang.com/">Sass</a>
             <a className="recipes-links" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Javascript</a>
+            <a className="recipes-links" href="https://jquery.com/">JQuery</a>
             <a className="recipes-links" href="https://github.com/ParsePlatform/parse-server-example">Parse</a>
           </div>
         </div>
@@ -86,11 +99,12 @@ var MajesticThaiComponent = React.createClass({
     return (
       <div className="majestic-thai">
         <div className="text-container col-md-8 col-xs-12">
-          <h1>Majestic Thai</h1>
+          <h1 className="majestic-thai-title"><p>Majestic Thai</p></h1>
           <p className="majestic-thai-text">Majestic Thai was a project where we
               had to create a new menu for a thai restaurant using React and Backbone.
               We were given no wireframes or design and were required to create our
-              own for the first time.</p>
+              own for the first time.
+          </p>
           <div className="majestic-thai-tech-bar">
             <p className="majestic-thai-tech">Technologies Used: </p>
             <a className="majestic-thai-links" href="https://getbootstrap.com/">Bootstrap</a>
@@ -98,6 +112,7 @@ var MajesticThaiComponent = React.createClass({
             <a className="majestic-thai-links" href="https://facebook.github.io/react/index.html">React</a>
             <a className="majestic-thai-links" href="http://sass-lang.com/">Sass</a>
             <a className="majestic-thai-links" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Javascript</a>
+            <a className="majestic-thai-links" href="https://jquery.com/">JQuery</a>
           </div>
         </div>
         <a className="col-md-4 col-xs-12" href="https://vierello.github.io/7.4-majestic-thai/"><img className="majestic-thai-image" src="./images/majestic-thai.png" /></a>
@@ -112,10 +127,11 @@ var Sk8SiteComponent = React.createClass({
       <div className="sk8-site">
         <a className="col-md-4 col-xs-12" href="http://vierello.github.io/2.2-sk8-site/"><img className="sk8-site-image" src="./images/sk8-site.png" /></a>
         <div className="text-container col-md-8 col-xs-12">
-          <h1>Sk8 Site</h1>
+          <h1 className="sk8-site-title"><p>Sk8 Site</p></h1>
           <p className="sk8-site-text">The Sk8 Site project had us use only css,
               html and sass to recreate a design composition provided. We also had
-              to make the website responsive using media queries.</p>
+              to make the website responsive using media queries.
+          </p>
           <div className="sk8-site-tech-bar">
             <p className="sk8-site-tech">Technologies Used: </p>
             <a className="sk8-site-links" href="http://sass-lang.com/">Sass</a>
@@ -133,20 +149,16 @@ var EtsyRecreationComponent = React.createClass({
     return (
       <div className="etsy-recreation">
         <div className="text-container col-md-8 col-xs-12">
-          <h1>Etsy Recreation</h1>
-          <p className="etsy-recreation-text">Everyone needs a budget! From the time I started earning my own money
-            while working my first job, until today, my parents have always told me
-            that I need a budget to help me make sure that I am taking care of all
-            my responsibilities. Budget Me is the final project I created during the
-            last few weeks of The Iron Yard. It was quite a challenge, and really
-            combined everything I learned during my time there. I used backbone,
-            bootstrap, and react mainly along with a couple smaller libraries that
-            I added in.</p>
+          <h1 className="etsy-recreation-title"><p>Etsy Recreation</p></h1>
+          <p className="etsy-recreation-text">The Etsy Recreation project involved
+              looking at the Etsy website to try to indentically recreate the page
+              using handlebars templates, along with using JSON data from the Etsy
+              API.
+          </p>
           <div className="etsy-recreation-tech-bar">
             <p className="etsy-recreation-tech">Technologies Used: </p>
-            <a className="etsy-recreation-links" href="https://getbootstrap.com/">Bootstrap</a>
+            <a className="etsy-recreation-links" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON">JSON</a>
             <a className="etsy-recreation-links" href="http://backbonejs.org/">Backbone</a>
-            <a className="etsy-recreation-links" href="https://facebook.github.io/react/index.html">React</a>
           </div>
         </div>
         <a className="col-md-4 col-xs-12" href="http://vierello.github.io/4.2-etsy-recreation/"><img className="etsy-recreation-image" src="./images/etsy.png" /></a>
