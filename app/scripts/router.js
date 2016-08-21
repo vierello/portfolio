20f73ebf1c5688e8c238c933b841a045b22c69a2
vierello <vierello@hotmail.com>
2016-08-21 18:13:22 -0400

    debug deploy

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,33 +1,33 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Backbone = require('backbone');
-
-var HomeComponent = require('./components/home.jsx').HomeComponent;
-var ResumeComponent = require('./components/resume.jsx');
-
-
-var Router = Backbone.Router.extend({
-  routes: {
-    '': 'index',
-    'home/': 'index',
-    'resume/': 'resume'
-  },
-
-  index: function() {
-    ReactDOM.render(
-      React.createElement(HomeComponent),
-      document.getElementById('container')
-    )
-  },
-
-  resume: function() {
-    ReactDOM.render(
-      React.createElement(ResumeComponent),
-      document.getElementById('container')
-    )
-  }
-});
-
-var router = new Router();
-
-module.exports = router;
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var Backbone = require('backbone');
+//
+// var HomeComponent = require('./components/home.jsx').HomeComponent;
+// var ResumeComponent = require('./components/resume.jsx');
+//
+//
+// var Router = Backbone.Router.extend({
+//   routes: {
+//     '': 'index',
+//     'home/': 'index',
+//     'resume/': 'resume'
+//   },
+//
+//   index: function() {
+//     ReactDOM.render(
+//       React.createElement(HomeComponent),
+//       document.getElementById('container')
+//     )
+//   },
+//
+//   resume: function() {
+//     ReactDOM.render(
+//       React.createElement(ResumeComponent),
+//       document.getElementById('container')
+//     )
+//   }
+// });
+//
+// var router = new Router();
+//
+// module.exports = router;
